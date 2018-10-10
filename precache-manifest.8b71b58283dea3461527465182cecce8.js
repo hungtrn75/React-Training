3fa530ae3d88a8f2cb6ba99728785c6cc294c849
Hung Tran <hungtrn75@gmail.com>
2018-10-10 16:34:36 +0700

    Updates

---
+++ precache-manifest.8b71b58283dea3461527465182cecce8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7a4a5b5723cddc67fdfe",
+    "url": "/static/js/main.7a4a5b57.chunk.js"
+  },
+  {
+    "revision": "aba2ebd6916be8d89210",
+    "url": "/static/js/1.aba2ebd6.chunk.js"
+  },
+  {
+    "revision": "7a4a5b5723cddc67fdfe",
+    "url": "/static/css/main.f19f397b.chunk.css"
+  },
+  {
+    "revision": "aba2ebd6916be8d89210",
+    "url": "/static/css/1.ab7da1d3.chunk.css"
+  },
+  {
+    "revision": "3cbda30ed26286f509477c0ef6140340",
+    "url": "/index.html"
+  }
+];
